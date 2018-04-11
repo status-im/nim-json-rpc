67eb771b4b1c664d6370f17afd2704442a6e4f6a
coffeepots <ryan@status.im>
2018-04-11 20:08:48 +0100

    WIP dummy async RPC calls

--- tests/testserverclient.nim
+++ tests/testserverclient.nim
@@ -1,10 +1,13 @@
-import ../eth-rpc / rpcclient, ../eth-rpc / rpcserver, asyncdispatch, json, unittest
+import ../eth-rpc / rpcclient, ../eth-rpc / rpcserver, asyncdispatch, json, unittest, ../eth-rpc/server/ethprocs
 
 # REVIEW: I'd like to see some dummy implementations of RPC calls handled in async fashion.
+proc myProc {.rpc.} =
+  return %"hello"
 
 when isMainModule:
   # create on localhost, default port
   var srv = newRpcServer("")
+  registerEthereumRpcs(srv)
   asyncCheck srv.serve()
 
   suite "RPC":
@@ -19,5 +22,8 @@ when isMainModule:
       test "SHA3":
         response = waitFor client.web3_sha3(%"abc")
         check response.result.getStr == "3A985DA74FE225B2045C172D6BD390BD855F086E3E9D525B46BFE24511431532"
+      response = waitFor client.call("myProc", %"abc")
+      echo response.result.getStr
+      
 
     waitFor main()
