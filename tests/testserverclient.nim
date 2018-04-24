d8fa488e24259ac4f4208bc4a69c08524fbbd90c
coffeepots <ryan@status.im>
2018-04-24 13:43:04 +0100

    Updated test to use new `on` macro

--- tests/testserverclient.nim
+++ tests/testserverclient.nim
@@ -1,19 +1,18 @@
 import ../eth-rpc / rpcclient, ../eth-rpc / rpcserver, 
-  asyncdispatch, json, unittest, tables
+  asyncdispatch, json, unittest, tables, ../eth-rpc / server / ethprocs
 
-# REVIEW: I'd like to see some dummy implementations of RPC calls handled in async fashion.
-proc myProc* {.rpc.} =
+# TODO: dummy implementations of RPC calls handled in async fashion.
+var srv = sharedRpcServer()
+srv.address = "localhost"
+srv.port = Port(8545)
+
+srv.on("myProc") do(input: string):
   # Custom async RPC call
-  return %"Hello"
+  result = %("Hello " & input)
 
-var srv = newRpcServer("")
-# This is required to automatically register `myProc` to new servers
-registerRpcs(srv)
 asyncCheck srv.serve
-# TODO: Avoid having to add procs twice, once for the ethprocs in newRpcServer,
-# and again with the extra `myProc` rpc
+
 when isMainModule:
-  # create on localhost, default port
   suite "RPC":
     proc main {.async.} =
       var client = newRpcClient()
@@ -24,11 +23,11 @@ when isMainModule:
         response = waitFor client.web3_clientVersion(newJNull())
         check response.result == %"Nimbus-RPC-Test"
       test "SHA3":
-        response = waitFor client.web3_sha3(%"abc")
+        response = waitFor client.web3_sha3(%["abc"])
         check response.result.getStr == "3A985DA74FE225B2045C172D6BD390BD855F086E3E9D525B46BFE24511431532"
       test "Custom RPC":
-        response = waitFor client.call("myProc", %"abc")
-        check response.result.getStr == "Hello"
+        response = waitFor client.call("myProc", %["abc"])
+        check response.result.getStr == "Hello abc"
       
 
     waitFor main()
