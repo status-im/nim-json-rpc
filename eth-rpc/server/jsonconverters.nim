e5e09718db79899eb6f50634999a3240b470d93c
coffeepots <ryan@status.im>
2018-05-09 14:58:21 +0100

    Don't export `bytes` iterator for `stint` types

--- eth-rpc/server/jsonconverters.nim
+++ eth-rpc/server/jsonconverters.nim
@@ -1,6 +1,6 @@
 import json, stint
 
-iterator bytes*(i: UInt256|Int256): byte =
+iterator bytes(i: UInt256|Int256): byte =
   let b = cast[ptr array[32, byte]](i.unsafeaddr)
   var pos = 0
   while pos < 32:
@@ -19,7 +19,6 @@ proc `%`*(n: Int256): JsonNode =
   for elem in n.bytes:
     result.add(%int(elem))
 
-proc `%`*(n: byte): JsonNode =
-  ## Generic constructor for JSON data. Creates a new `JInt JsonNode`.
+proc `%`*(n: byte{not lit}): JsonNode =
   result = newJInt(int(n))
 
