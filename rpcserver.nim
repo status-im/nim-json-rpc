981526a966be1b890f4d1ff467dea26af77a172c
coffeepots <ryan@status.im>
2018-05-22 20:43:08 +0100

    export ethtypes in server

--- rpcserver.nim
+++ rpcserver.nim
@@ -2,5 +2,6 @@ import
   eth-rpc / server / servertypes,
   eth-rpc / server / rpcconsts,
   eth-rpc / server / serverdispatch,
-  eth-rpc / server / ethprocs
-export servertypes, rpcconsts, serverdispatch, ethprocs
+  eth-rpc / server / ethprocs,
+  eth-rpc / ethtypes
+export servertypes, rpcconsts, serverdispatch, ethprocs, ethtypes
