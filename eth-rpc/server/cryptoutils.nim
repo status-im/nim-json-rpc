ef53b52c9e41ed089af64eb045e5383b4ddbe1e5
coffeepots <ryan@status.im>
2018-05-23 18:37:26 +0100

    cryptoutils is no longer needed

--- eth-rpc/server/cryptoutils.nim
+++
@@ -1,9 +0,0 @@
-import nimcrypto
-
-proc k256*(data: string): string =
-  # do not convert, assume string is data
-  # REVIEW: Nimcrypto has a one-liner for the code here: sha3_256.digest(data)
-  var k = sha3_256()
-  k.init
-  k.update(cast[ptr uint8](data[0].unsafeaddr), data.len.uint)
-  result = $finish(k)
