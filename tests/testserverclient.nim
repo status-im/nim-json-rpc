efeb972763a3dcb08e2355c458f9cf9b8db0d196
coffeepots <coffeepotsgh@gmail.com>
2018-06-05 10:45:54 +0100

    Update tests to use async better, no shared server

--- tests/testserverclient.nim
+++ tests/testserverclient.nim
@@ -1,28 +1,20 @@
 import  ../ rpcclient, ../ rpcserver
-import unittest, asyncdispatch, json
+import unittest, json
 
 var srv = newRpcServer()
 srv.address = "localhost"
 srv.port = Port(8545)
+var client = newRpcClient()
 
-proc makeProc(server: RpcServer) =
-  server.rpc("myProc") do(input: string, data: array[0..3, int]):
-    result = %("Hello " & input & " data: " & $data)
+# Create RPC on server
+srv.rpc("myProc") do(input: string, data: array[0..3, int]):
+  result = %("Hello " & input & " data: " & $data)
 
 asyncCheck srv.serve
+waitFor client.connect("localhost", Port(8545))
 
-srv.makeProc
-
+# TODO: When an error occurs during a test, stop the server
 suite "Server/Client RPC":
-  proc main {.async.} =
-    var client = newRpcClient()
-    await client.connect("localhost", Port(8545))
-
-    test "Custom RPC":
-      # Custom async RPC call
-      var response = waitFor client.call("myProc", %[%"abc", %[1, 2, 3, 4]])
-      check response.result.getStr == "Hello abc data: [1, 2, 3, 4]"
-
-  # TODO: When an error occurs during a test, stop the server
-  asyncCheck main()
-  
+  test "Custom RPC":
+    var r = waitFor client.call("myProc", %[%"abc", %[1, 2, 3, 4]])
+    check r.result.getStr == "Hello abc data: [1, 2, 3, 4]"
