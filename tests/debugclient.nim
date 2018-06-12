e39008fe4a3b4697a6275d3bb659e405399ea0a2
coffeepots <coffeepotsgh@gmail.com>
2018-06-12 19:28:41 +0100

    Removed extraneous debug output

--- tests/debugclient.nim
+++ tests/debugclient.nim
@@ -6,14 +6,15 @@ proc rawCall*(self: RpcClient, name: string,
   # For debug purposes only
   let id = $self.nextId
   self.nextId.inc
-  debug "Sending message", msg = msg
+
   var s = msg & "\c\l"
   let res = await self.transp.write(s)
+  
   debug "Receiving length assert", value = res, length = len(msg), lengthDifferent = res != len(msg)
 
   # completed by processMessage.
   var newFut = newFuture[Response]()
   # add to awaiting responses
   self.awaiting[id] = newFut
-
+  
   result = await newFut
