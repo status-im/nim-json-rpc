93c7a5577e743cd11f40611a204b2e414b204a22
coffeepots <coffeepotsgh@gmail.com>
2018-06-12 18:09:00 +0100

    `var` strings only for transport

--- tests/debugclient.nim
+++ tests/debugclient.nim
@@ -7,7 +7,8 @@ proc rawCall*(self: RpcClient, name: string,
   let id = $self.nextId
   self.nextId.inc
   debug "Sending message", msg = msg
-  let res = await self.transp.write(msg & "\c\l")
+  var s = msg & "\c\l"
+  let res = await self.transp.write(s)
   debug "Receiving length assert", value = res, length = len(msg), lengthDifferent = res != len(msg)
 
   # completed by processMessage.
