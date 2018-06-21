b632cbfb25b5a9a0accb68a54cee604f072c35a0
coffeepots <coffeepotsgh@gmail.com>
2018-06-21 18:39:47 +0100

    Added HTTP afterRead code (WIP)

--- rpchttpservers.nim
+++ rpchttpservers.nim
@@ -8,8 +8,11 @@ defineRpcTransport(httpProcessClient):
   write:
     let
       msg = &"Host: {$client.localAddress} Content-Type: application/json-rpc Content-Length: {$value.len} {value}"
-    debug "Http stream", msg = msg
+    debug "Http write", msg = msg
     client.write(msg)
+  afterRead:
+    # TODO: read: remove http to allow json validation
+    debug "Http read", msg = value
 
 proc newRpcHttpServer*(addresses: openarray[TransportAddress]): RpcHttpServer = 
   ## Create new server and assign it to addresses ``addresses``.
