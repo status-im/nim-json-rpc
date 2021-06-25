ccc310b00d4c030db916077fc50ed97128cd60f6
KonradStaniec <konrad.staniec@gmail.com>
2021-06-25 11:36:09 +0200

    [FEATURE] Add missing testcase, enable effects tracking

--- json_rpc/rpcproxy.nim
+++ json_rpc/rpcproxy.nim
@@ -1,3 +1,5 @@
+{.push raises: [Defect].}
+
 import 
   ./servers/[httpserver],
   ./clients/[httpclient]
@@ -9,12 +11,12 @@ type RpcHttpProxy* = ref object of RootRef
 proc proxyCall(client: RpcHttpClient): ProxyCall =
   return proc (name: string, params: JsonNode): Future[Response] = client.call(name, params)
 
-proc new*(T: type RpcHttpProxy, listenAddresses: openArray[string]): T = 
+proc new*(T: type RpcHttpProxy, listenAddresses: openArray[string]): T {.raises: [Defect, CatchableError].}= 
   let client = newRpcHttpClient()
   let router = RpcRouter.init()
   T(rpcHttpClient: client, rpcHttpServer: newRpcHttpServer(listenAddresses, router))
 
-proc newRpcHttpProxy*(listenAddresses: openArray[string]): RpcHttpProxy =
+proc newRpcHttpProxy*(listenAddresses: openArray[string]): RpcHttpProxy {.raises: [Defect, CatchableError].} =
   RpcHttpProxy.new(listenAddresses)
 
 proc start*(proxy:RpcHttpProxy, proxyServerUrl: string) {.async.} =
@@ -28,10 +30,10 @@ proc start*(proxy:RpcHttpProxy, proxyServerAddress: string, proxyServerPort: Por
 template rpc*(server: RpcHttpProxy, path: string, body: untyped): untyped =
   server.rpcHttpServer.rpc(path, body)
 
-proc registerProxyMethod*(proxy: var RpcHttpProxy, methodName: string) = 
-  proxy.rpcHttpServer.registerProxyCall(methodName, proxyCall(proxy.rpcHttpClient))
+proc registerProxyMethod*(proxy: var RpcHttpProxy, methodName: string) {.raises: [Defect, CatchableError].} = 
+ proxy.rpcHttpServer.registerProxyCall(methodName, proxyCall(proxy.rpcHttpClient))
 
-proc stop*(rpcHttpProxy: RpcHttpProxy) =
+proc stop*(rpcHttpProxy: RpcHttpProxy) {.raises: [Defect, CatchableError].} =
   rpcHttpProxy.rpcHttpServer.stop()
 
 proc closeWait*(rpcHttpProxy: RpcHttpProxy) {.async.} =
--- tests/testproxy.nim
+++ tests/testproxy.nim
@@ -9,17 +9,29 @@ let proxySrvAddress = "localhost:8546"
 var proxy = newRpcHttpProxy([proxySrvAddress])
 
 var client = newRpcHttpClient()
+let duplicatedProcedureName = "duplicated"
 
 # Create RPC on server
 srv.rpc("myProc") do(input: string, data: array[0..3, int]):
   return %("Hello " & input & " data: " & $data)
 
+# registration of duplicated procedures needs to happen in separate scope as otherwise macro does not compile, due
+# to redefinition of procedure name in scope (even if it happens on two different objects)
+block:
+  srv.rpc(duplicatedProcedureName) do()-> string:
+    return "server"
+
 # Create RPC on proxy server
 proxy.registerProxyMethod("myProc")
+proxy.registerProxyMethod(duplicatedProcedureName)
 
 # Create standard handler on server
 proxy.rpc("myProc1") do(input: string, data: array[0..3, int]):
   return %("Hello " & input & " data: " & $data)
+
+block:
+  proxy.rpc(duplicatedProcedureName) do()-> string:
+    return "proxy"
 
 srv.start()
 waitFor proxy.start("localhost", Port(8545))
@@ -32,6 +44,9 @@ suite "Proxy RPC":
   test "Successful RPC call no proxy":
     let r = waitFor client.call("myProc1", %[%"abc", %[1, 2, 3, 4]])
     check r.getStr == "Hello abc data: [1, 2, 3, 4]"
+  test "Call local method instead of proxy in case of conflict":
+    let r = waitFor client.call("duplicated", %[])
+    check r.getStr == "proxy"
   test "Missing params":
     expect(CatchableError):
       discard waitFor client.call("myProc", %[%"abc"])
