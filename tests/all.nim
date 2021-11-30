5a840c15d7917a7c791f4963a254d16cda6e6d87
Zahary Karadjov <zahary@gmail.com>
2021-11-30 02:45:36 +0200

    Fix #117

--- tests/all.nim
+++ tests/all.nim
@@ -4,12 +4,7 @@ import
   ../json_rpc/clients/config
 
 import
-  testrpcmacro, testethcalls, testhttp
-
-when not useNews:
-  # TODO The websock server doesn't interop properly
-  #      with the news client at the moment
-  import testserverclient
+  testrpcmacro, testethcalls, testhttp, testserverclient
 
 when not useNews:
   # The proxy implementation is based on websock
