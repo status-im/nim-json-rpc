c3769f91307d954542737d4294d90f3906bcd767
jangko <jangko128@gmail.com>
2023-12-14 08:34:13 +0700

    Add copyright to source file

--- json_rpc/clients/websocketclientimpl.nim
+++ json_rpc/clients/websocketclientimpl.nim
@@ -1,9 +1,18 @@
+# json-rpc
+# Copyright (c) 2019-2023 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
 import
   std/[uri, strutils],
   pkg/websock/[websock, extensions/compression/deflate],
   pkg/[chronos, chronos/apps/http/httptable, chronicles],
   stew/byteutils
-  
+
 # avoid clash between Json.encode and Base64Pad.encode
 import ../client except encode
 
--- json_rpc/rpcclient.nim
+++ json_rpc/rpcclient.nim
@@ -1,3 +1,12 @@
+# json-rpc
+# Copyright (c) 2019-2023 Status Research & Development GmbH
+# Licensed under either of
+#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE))
+#  * MIT license ([LICENSE-MIT](LICENSE-MIT))
+# at your option.
+# This file may not be copied, modified, or distributed except according to
+# those terms.
+
 import client
 import clients/[socketclient, httpclient, websocketclient]
 export client, socketclient, httpclient, websocketclient
