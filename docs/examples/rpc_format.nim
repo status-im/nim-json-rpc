71f76f2a5f587bf6b70fcdc67da26a55a90ebb5f
nitely <ecastroborsani@gmail.com>
2026-06-29 22:08:38 -0300

    wip

--- docs/examples/rpc_format.nim
+++ docs/examples/rpc_format.nim
@@ -19,4 +19,31 @@ createJsonFlavor RpcConv,
   skipNullFields = true # Skip optional fields==null in Reader
 # ANCHOR_END: FormatRpcConv
 
+# ANCHOR: FormatUserInfo
+type
+  UserInfo* = object
+    name*: string
+    bio*: string
+
+UserInfo.useDefaultSerializationIn RpcConv
+# ANCHOR_END: FormatUserInfo
+
+# ANCHOR: FormatUploadData
+type
+  UploadData* = object
+    path*: string
+    public*: bool
+
+proc readValue*(
+    reader: var RpcConv.Reader, value: var UploadData
+) {.gcsafe, raises: [IOError, SerializationError].} =
+  let path = reader.readValue(string)
+  value = UploadData(path: path, public: false)
+
+proc writeValue*(
+    writer: var RpcConv.Writer, value: UploadData
+) {.gcsafe, raises: [IOError].} =
+  writer.writeValue value.path
+# ANCHOR_END: FormatUploadData
+
 # ANCHOR_END: All
